094b52a135dc9e46c08c4dd6ba5d9ccac6317df5
rekunolya <rekun.olya@gmail.com>
2024-12-19 01:39:52 +0300

    feat: add a solution to task getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -137,8 +137,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const result = arr.reduce((sum, current) => sum + current, 0);
+  const average = result / arr.length;
+  return Math.round(average * 100) / 100;
 }
 
 /**
