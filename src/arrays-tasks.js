0a58021335592fd63aba12b95b4327ab2059b974
rekunolya <rekun.olya@gmail.com>
2024-12-19 01:21:13 +0300

    feat: add a solution to task sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -41,8 +41,15 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let arr = [];
+  if (arr1.length > arr2.length) {
+    arr = arr1.map((el, ind) => el + (arr2[ind] || 0));
+  } else {
+    arr = arr2.map((el, ind) => el + (arr1[ind] || 0));
+  }
+
+  return arr;
 }
 
 /**
